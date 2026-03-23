a0a52c71338f9be44b8a35184d1b9c537a7f0a38
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-03-24 07:14:15 +0900

    Render basic full year calendar - 12 months!

--- src/react-app/components/calendar.tsx
+++ src/react-app/components/calendar.tsx
@@ -5,16 +5,20 @@ export default function Calendar() {
     const daysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate();
 
     return (
-        <div className="w-80 p-10">
-            <h1 className="text-center font-bold text-lg border">{new Intl.DateTimeFormat("en-US", {
-                year: 'numeric',
-                month: 'long',
-            }).format(today)}</h1>
-            <div className="grid grid-cols-7 border rounded-md p-2">
-                {[...Array(daysInMonth(today.getFullYear(), today.getMonth() + 1))].map((_, i) => (
-                    <p key={i} className="text-center hover:bg-accent hover:text-accent-foreground rounded-full p-1">{i + 1}</p>
-                ))}
-            </div>
+        <div className="grid grid-cols-3 gap-6 px-8 py-4">
+            {[...Array(12)].map((_, i) => (
+                <div className="border rounded-md">
+                    <h1 className="text-center font-bold text-lg border-b">
+                        {new Intl.DateTimeFormat("en-US", {
+                            month: 'long',
+                        }).format(new Date(today.getFullYear(), i))}</h1>
+                    <div className="grid grid-cols-7">
+                        {[...Array(daysInMonth(today.getFullYear(), i + 1))].map((_, j) => (
+                            <p key={j} className="text-center hover:bg-accent hover:text-accent-foreground rounded-full p-1">{j + 1}</p>
+                        ))}
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
