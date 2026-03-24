b34eb4df0c94cf5739c0a01984ef9b0b3079c265
ポテト ^. .^₎ฅ <151442433+CheapNightbot@users.noreply.github.com>
2026-03-24 16:08:21 +0900

    Add weekdays, align days with weekdays correctly

--- src/react-app/components/calendar.tsx
+++ src/react-app/components/calendar.tsx
@@ -3,18 +3,34 @@
 export default function Calendar() {
     const today = new Date();
     const daysInMonth = (year: number, month: number) => new Date(year, month, 0).getDate();
+    const weekdays: string[] = [];
+
+
+    for (let i = 0; i < 7; i++) {
+        let weekday = new Intl.DateTimeFormat("en-US", {
+            weekday: "short",
+        }).format(new Date(2026, 1, i + 1));
+        weekdays.push(weekday)
+    }
 
     return (
-        <div className="grid grid-cols-3 gap-6 px-8 py-4">
-            {[...Array(12)].map((_, i) => (
+        <div className="grid grid-cols-3 gap-6 px-8 py-4 lg:max-w-5xl mx-auto">
+            {[...Array(12)].map((_, month) => (
                 <div className="border rounded-md">
                     <h1 className="text-center font-bold text-lg border-b">
                         {new Intl.DateTimeFormat("en-US", {
                             month: 'long',
-                        }).format(new Date(today.getFullYear(), i))}</h1>
+                        }).format(new Date(today.getFullYear(), month))}
+                    </h1>
+                    <h2 className="grid grid-cols-7 text-center border-b">
+                        {weekdays.map((weekday, i) => (
+                            <span key={i}>{weekday}</span>
+                        ))}
+                    </h2>
                     <div className="grid grid-cols-7">
-                        {[...Array(daysInMonth(today.getFullYear(), i + 1))].map((_, j) => (
-                            <p key={j} className="text-center hover:bg-accent hover:text-accent-foreground rounded-full p-1">{j + 1}</p>
+                        {[...Array(new Date(today.getFullYear(), month, 1).getDay())].map((_, i) => <div key={i}></div>)}
+                        {[...Array(daysInMonth(today.getFullYear(), month + 1))].map((_, day) => (
+                            <p key={day} className="text-center hover:bg-accent hover:text-accent-foreground rounded-full p-1">{day + 1}</p>
                         ))}
                     </div>
                 </div>
